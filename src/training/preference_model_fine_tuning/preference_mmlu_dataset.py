660e2b0f514b909e267d9af9e6d1fc5ae485b6ea
Qiping Pan <panqiping@outlook.com>
2026-01-26 22:31:29 -0800

    clinc-150

--- src/training/preference_model_fine_tuning/preference_mmlu_dataset.py
+++ src/training/preference_model_fine_tuning/preference_mmlu_dataset.py
@@ -2,17 +2,57 @@
 from pathlib import Path
 from typing import Iterable, List
 from datasets import load_dataset
-from training.preference_model_fine_tuning.dataset_pipeline_sharegpt import (
+from dataset_pipeline_sharegpt import (
     Turn,
     ShareGPTConversation,
     RoutePolicy,
 )
-from training.preference_model_fine_tuning.preference_model_ft_reranking import (
+from preference_model_ft_reranking import (
     PreferenceTrainingExample,
 )
 
 
-def build_mmlu_eval_examples():
+def build_clinc150_training_examples(use_domain_only: bool = True):
+    dataset = load_dataset("contemmcm/clinc150", split="complete")
+    examples: List[PreferenceTrainingExample] = []
+
+    if use_domain_only:
+        all_labels = dataset.unique("domain")
+    else:
+        all_labels = dataset.unique("intent")
+    for item in dataset:
+        question = item["text"]
+        # we only care about domain for routing purpose
+        label = item["domain"] if use_domain_only else item["intent"]
+        sample_id = item["id"]
+
+        conversation = ShareGPTConversation(
+            sample_id=sample_id,
+            messages=[
+                Turn(role="user", content=question),
+            ],
+        )
+
+        truth_policy = RoutePolicy(
+            label=label,
+            description=label,
+        )
+
+        negative_policies = [
+            RoutePolicy(label=l, description=l) for l in all_labels if l != label
+        ]
+
+        examples.append(
+            PreferenceTrainingExample(
+                conversation=conversation,
+                truth_policy=truth_policy,
+                negative_policies=negative_policies,
+            )
+        )
+    return examples
+
+
+def build_mmlu_training_examples():
     dataset = load_dataset("TIGER-Lab/MMLU-Pro", split="test")
     examples: List[PreferenceTrainingExample] = []
 
@@ -21,7 +61,7 @@ def build_mmlu_eval_examples():
         question = item["question"]
         # we only care about category for routing purpose
         category = item["category"]
-        sample_id = item["id"]
+        sample_id = item["question_id"]
 
         conversation = ShareGPTConversation(
             sample_id=sample_id,
@@ -48,4 +88,8 @@ def build_mmlu_eval_examples():
                 negative_policies=negative_policies,
             )
         )
-    print(len(examples), "MMLU eval examples built.")
+    return examples
+
+
+if __name__ == "__main__":
+    build_mmlu_training_examples()
